231241492b07dc9a1782f0e87191bbcff360ca8a
kenzan8000 <kenzan8000@gmail.com>
2017-07-25 23:35:40 -0700

    initial commit

---
+++ Mac/RSSReader/Classes/AppDelegate.swift
@@ -0,0 +1,18 @@
+import Cocoa
+
+
+/// MARK: - AppDelegate
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    /// MARK: - property
+
+    /// MARK: - life cycle
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+    }
+
+}
+
---
+++ Mac/RSSReader/Classes/ViewController.swift
@@ -0,0 +1,19 @@
+import Cocoa
+
+
+/// MARK: - ViewController
+class ViewController: NSViewController {
+
+    /// MARK: - property
+    override var representedObject: Any? {
+        didSet {
+        }
+    }
+
+    /// MARK: - life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
