febba5b901ef0229c645c577672aff266089cc66
kenzan8000 <kenzan8000@gmail.com>
2017-08-03 23:42:02 -0700

    add RSSClient

---
+++ Mac/RSSReader/Classes/RSS/RSSClient.swift
@@ -0,0 +1,20 @@
+/// MARK: - RSSClient
+class RSSClient: NSObject {
+
+    /// MARK: - property
+    static let sharedInstance = VGMapDataDownloader()
+
+
+    /// MARK: - init
+
+    init() {
+    }
+
+
+    /// MARK: - destruction
+
+    deinit {
+    }
+
+}
+
