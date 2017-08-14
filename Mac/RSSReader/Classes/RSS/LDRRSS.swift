dc892f1a821c0614c75304953b17e4eec75363ea
kenzan8000 <kenzan8000@gmail.com>
2017-08-13 23:47:05 -0700

    update LDRRSS

--- Mac/RSSReader/Classes/RSS/LDRRSS.swift
+++ Mac/RSSReader/Classes/RSS/LDRRSS.swift
@@ -8,12 +8,33 @@ struct LDRRSS: XMLIndexerDeserializable {
         // feed
     let title: String
     let link: URL
+    let description: String?
         // entries
 
     static func deserialize(_ node: XMLIndexer) throws -> LDRRSS {
+        // RSS 1.0
+        do {
+            return try LDRRSS(
+                title: node["rdf:RDF"]["channel"]["title"].value(),
+                link: URL(string: node["rdf:RDF"]["channel"]["link"].value())!,
+                description: node["rdf:RDF"]["channel"]["description"].value()
+            )
+        }
+        catch { }
+        // RSS 2.0
+        do {
+             return try LDRRSS(
+                title: node["rss"]["channel"]["title"].value(),
+                link: URL(string: node["rss"]["channel"]["link"].value())!,
+                description: node["rss"]["channel"]["description"].value()
+             )
+        }
+        catch { }
+        // Atom
         return try LDRRSS(
-           title: node["rss"]["title"].value(),
-           link: URL(string: node["rss"]["link"].value())!
+            title: node["feed"]["title"].value(),
+            link: URL(string: node["feed"]["link"].value(ofAttribute: "alternate"))!,
+            description: node["feed"]["subtitle"].value()
         )
     }
 }
